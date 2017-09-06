8c9565e241a17cdc61a07bf8c05c1dab7f317b44
iiiedu10 <iiiedu10@iiiedu10.local>
2017-09-06 10:48:15 +0800

    Initial Commit

---
+++ Shappo01/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Shappo01
+//
+//  Created by iiiedu10 on 2017/9/6.
+//  Copyright © 2017年 iiiedu_10. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Hey, Shappo~Shappo~")
+
+var a = Int.max
+print(a)
+var b = a - 1
+print(b)
+
